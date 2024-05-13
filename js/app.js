9b86268b3e8587137e760b7d912cb7dd59c8e2f4
viacheslavk55 <525vkostenko@frhsd.com>
2024-05-13 10:36:53 -0400

    added to do

--- js/app.js
+++ js/app.js
@@ -3,5 +3,31 @@
 const clear = document.querySelector(".clear")
 const dateElement = document.getElementById('date')
 const list = document.getElementById('list')
+
+function addToDo(toDo) {
+    const text = `<li class="item">
+                <i class="co fa fa-circle-thin" job="complete"></i>
+                <p class="text">${toDo}</p>
+                <i class="de fa fa-trash-o" job="delete"></i>
+             </li>`
+
+    const position = "beforeend"
+
+    list.insertAdjacentHTML(position, text)
+}
+addToDo("Drink Coffee")
+
+
+
 const input = document.getElementById('input')
+
+document.addEventListener("keyup", function(event){
+    if(event.keyCode == 13){
+        const toDo = input.value
+        if(toDo){
+            addToDo(toDo)
+        }
+        input.value = ""
+    }
+})
 
